f2459cbed2defec81741f3555a8e93883650dea6
istekhar khan <istekharkhan46@gmail.com>
2023-04-24 21:18:03 +0530

    added syntax-error msg on wrong expression

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let buttons = document.querySelectorAll(".btn");
 let s = ""
 Array.from(buttons).forEach((item) => {
-    
+
     item.addEventListener('click', (e) => {
 
         if (e.target.innerHTML == '=') {
-            s = eval(s)
-            document.querySelector("input").value = s;
+            try {
+                s = eval(s)
+                document.querySelector("input").value = s;
+            }
+            catch (e) {
 
+                document.querySelector("input").value = "SyntaxError";
+            }
+            
         }
-
         else if (e.target.innerHTML == "AC") {
             s = ""
             document.querySelector("input").value = s;
-            
+
         }
-        
-        else if(e.target.innerHTML == "DEL"){
-            
-            document.querySelector("input").value = s.substring(0,s.length-1);
-            s= s.substring(0,s.length-1)
+
+        else if (e.target.innerHTML == "DEL") {
+
+            document.querySelector("input").value = s.substring(0, s.length - 1);
+            s = s.substring(0, s.length - 1)
         }
 
         else {
